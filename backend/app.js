d8f49543c22f636b9e058dfe229d2bb59d3d452e
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-06 13:42:22 +0200

    Add initial MongoDB connection setup using dotenv and update dependencies.

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,39 @@
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 
-const productRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
+const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   // Set CORS headers so that the React SPA is able to communicate with this server
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS",
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/products', productRoutes);
-app.use('/', authRoutes);
+app.use("/products", productRoutes);
+app.use("/", authRoutes);
+
+MongoClient.connect(process.env.MONGODB_URI)
+  .then((client) => {
+    console.log("Connected to MongoDB");
+    client.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3100);
